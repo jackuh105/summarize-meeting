72159dea9d14d0d0d6ba406985f272d4b7128d07
Jack Cheang <uhcheang@safp.gov.mo>
2025-12-04 11:06:02 +0800

    change to asyncio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import io
 import os
 import httpx
+import asyncio
 import argparse
 import warnings
 from dotenv import load_dotenv
@@ -10,13 +11,17 @@ import pyannote.audio.core.task
 from pyannote.audio import Pipeline
 from pyannote.audio.pipelines.utils.hook import ProgressHook
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
 # ignore torchaudio warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="torchaudio")
+
 # settings
 env_path = os.path.join(os.getcwd(), ".env")
 load_dotenv(env_path)
+PADDING = 0.3
 MAX_WORKERS = 3
 STT_TIMEOUT = 60.0
+
 # add pyannote.audio to torch's safe global list
 torch.serialization.add_safe_globals([
     pyannote.audio.core.task.Specifications,
@@ -69,37 +74,53 @@ def merge_segments(speaker_tracks, max_gap=2.0):
     merged_results.sort(key=lambda x: x["start"])
     return merged_results
 
-def transcribe_segment(client, segment, waveform, sample_rate, padding=0.3):
+def prepare_audio_buffer(waveform, start_time, end_time, sample_rate):
     try:
         total_frames = waveform.shape[1]
-        start_time = max(0, segment["start"] - padding)
-        end_time = segment["end"] + padding
-        start_frame = int(start_time * sample_rate)
-        end_frame = int(end_time * sample_rate)
-        if end_frame > total_frames: end_frame = total_frames # boundary check
+        s_time = max(0, start_time - PADDING)
+        e_time = end_time + PADDING
+        start_frame = int(s_time * sample_rate)
+        end_frame = int(e_time * sample_rate)
+        if end_frame > total_frames: end_frame = total_frames
         segment_waveform = waveform[:, start_frame:end_frame]
-
         buffer = io.BytesIO()
         torchaudio.save(buffer, segment_waveform, sample_rate, format="wav")
         buffer.seek(0)
-
-        files = { 'file': (f"{segment['speaker']}.wav", buffer, 'audio/wav') }
-        data = {
-            'model_name': 'sensevoice',
-            'response_format': 'json',
-            'temperature': 0.0
-        }
-        response = client.post("/audio/transcriptions", files=files, data=data) # openai api compatible format
-        if response.status_code == 200:
-            result = response.json()
-            text = result.get("text", "").strip()
-            return {**segment, "text": text}
-        else:
-            print(f"Error transcribing segment: {response.status_code}")
-            return {**segment, "text": "[Transcribe Error]"}
+        return buffer
     except Exception as e:
-        print(f"Error processing segment: {segment['start']:.1f}s: {e}")
-        return {**segment, "text": "[Exception]"}
+        print(f"Error in prepare_audio_buffer: {e}")
+        return None
+
+async def transcribe_segment(sem, client, segment, waveform, sample_rate):
+    async with sem:
+        try:
+            buffer = await asyncio.to_thread(
+                prepare_audio_buffer,
+                waveform,
+                segment["start"],
+                segment["end"],
+                sample_rate
+            )
+            if buffer is None:
+                return {**segment, "text": "[Audio Error]"}
+
+            files = { 'file': (f"{segment['speaker']}.wav", buffer, 'audio/wav') }
+            data = {
+                'model_name': 'sensevoice',
+                'response_format': 'json',
+                'temperature': 0.0
+            }
+            response = await client.post("/audio/transcriptions", files=files, data=data) # openai api compatible format
+            if response.status_code == 200:
+                result = response.json()
+                text = result.get("text", "").strip()
+                return {**segment, "text": text}
+            else:
+                print(f"Error transcribing segment: {response.status_code}")
+                return {**segment, "text": "[Transcribe Error]"}
+        except Exception as e:
+            print(f"Error processing segment: {segment['start']:.1f}s: {e}")
+            return {**segment, "text": "[Exception]"}
 
 def generate_srt(segments):
     srt_output = []
@@ -131,9 +152,7 @@ def export_audio_segments(audio_path, segments, output_dir="segments", padding=0
 
         start_frame = int(start_time * sample_rate)
         end_frame = int(end_time * sample_rate)
-        # boundary check
-        if end_frame > total_frames:
-            end_frame = total_frames
+        if end_frame > total_frames: end_frame = total_frames
 
         segment_waveform = waveform[:, start_frame:end_frame]
         original_duration = seg["end"] - seg["start"]
@@ -141,7 +160,7 @@ def export_audio_segments(audio_path, segments, output_dir="segments", padding=0
         save_path = os.path.join(output_dir, filename)
         torchaudio.save(save_path, segment_waveform, sample_rate)
 
-def main(input_path):
+async def main(input_path):
     print("1. Running Diarization...")
     with ProgressHook() as hook:
         output = pipeline(input_path, hook=hook)
@@ -150,22 +169,22 @@ def main(input_path):
     speaker_tracks = get_speaker_tracks(output)
     cleaned_tracks = merge_segments(speaker_tracks, max_gap=2.0)
 
-    print(f"3. Loading Audio into Memory ({audio_file})...")
-    waveform, sample_rate = torchaudio.load(audio_file)
+    print(f"3. Loading Audio into Memory ({input_path})...")
+    waveform, sample_rate = torchaudio.load(input_path)
 
     print(f"4. Transcribing {len(cleaned_tracks)} segments (Parallel Workers={MAX_WORKERS})...")
     final_segments = []
-    with httpx.Client(base_url=os.getenv("STT_BASE_API"), timeout=STT_TIMEOUT) as client:
-        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-            future_to_segment = {
-                executor.submit(transcribe_segment, client, seg, waveform,sample_rate): seg
-                for seg in cleaned_tracks
-            }
-            for i, future in enumerate(as_completed(future_to_segment)):
-                seg = future.result()
-                final_segments.append(seg)
-                if i % 5 == 0:
-                    print(f"Processed {i+1}/{len(cleaned_tracks)} segments...")
+    sem = asyncio.Semaphore(MAX_WORKERS)
+    async with httpx.AsyncClient(base_url=os.getenv("STT_BASE_API"), timeout=STT_TIMEOUT) as client:
+        tasks = [
+            transcribe_segment(sem, client, seg, waveform, sample_rate)
+            for seg in cleaned_tracks
+        ]
+        for i, task in enumerate(asyncio.as_completed(tasks)):
+            seg = await task
+            final_segments.append(seg)
+            if i % 5 == 0 or (i + 1) == len(cleaned_tracks):
+                print(f"Processed {i+1}/{len(cleaned_tracks)} segments...")
     final_segments.sort(key=lambda x: x["start"])
 
     print("5. Generating SRT...")
@@ -174,12 +193,10 @@ def main(input_path):
     with open(output_srt_path, "w", encoding="utf-8") as f:
         f.write(srt_content)
     print(f"Done! SRT saved to: {output_srt_path}")
-    print("\n--- Preview ---")
-    print("\n".join(srt_content.split("\n")[:10]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=str, required=True, help="Input audio file path")
     args = parser.parse_args()
 
-    main(args.input)
+    asyncio.run(main(args.input))
